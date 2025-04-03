6d864e54261eae58abe47e1e2dc55dec92e605ab
Nabin Ghimire <ghimiren2057@gmail.com>
2025-04-03 13:53:58 +0545

    add setTests for matchMedia function

--- setupTests.ts
+++ setupTests.ts
@@ -1,1 +1,23 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
+import {beforeEach, vi} from 'vitest';
+
+beforeEach(()=>{
+
+  const matchMediaMock=vi.fn().mockImplementation((query) => ({
+  
+  matches: false,
+  media: query,
+  onchange: null, 
+  addListener: vi.fn(), // Deprecated
+  removeListener: vi.fn(), // Deprecated
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn()
+  
+  }));
+
+  vi.stubGlobal('matchMedia', matchMediaMock);
+  vi.stubGlobal('getComputedStyle',  () => ({   //I changed this line because it was not working with sir's code;
+    getPropertyValue: () => '', // Return an empty string or mock necessary values
+  }));
+});
