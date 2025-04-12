d4c67a5dd607620eaf336313a587b83eb0a7d6d3
Nabin Ghimire <ghimiren2057@gmail.com>
2025-04-12 23:03:10 +0545

    make protected route by letting access page only to the logged in user

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { userAuthStore } from "../store"
 
 const Dashboard = () => {
+  const { user } = userAuthStore();
+  if (user === null) {
+    return <Navigate to="/auth/login" replace={true} />
+  }
   return (
     <div>
       <h1>DashBoard</h1>
