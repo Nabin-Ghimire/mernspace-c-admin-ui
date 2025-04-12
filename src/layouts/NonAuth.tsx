bba28ca17acf494daf5a5e497396aaffc92bae34
Nabin Ghimire <ghimiren2057@gmail.com>
2025-04-12 23:03:35 +0545

    make protected route by letting access page only to the not logged in user

--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { userAuthStore } from "../store"
 
 const NonAuth = () => {
+  const { user } = userAuthStore();
+  if (user !== null) {
+    return <Navigate to="/" replace={true} />
+  }
   return (
     <div>
       <h1>NonAuth</h1>
