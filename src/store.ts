510ae7660f1d406dac5a5efad0b3ef38039c9ddf
Nabin Ghimire <ghimiren2057@gmail.com>
2025-04-14 23:11:57 +0545

    storing tenant info

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,18 @@
 import { create } from "zustand";
 
+interface Tenant{
+  id:number;
+  name:string;
+  address:string;
+}
+
 export interface User{
   id:number;
   firstName:string;
   lastName:string;
   email:string;
   role:string;
+  tenant?:Tenant;
 }
 
 interface AuthState{
