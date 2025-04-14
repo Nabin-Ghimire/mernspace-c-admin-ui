1bfc09855dec23de947bb5822209962566906202
Nabin Ghimire <ghimiren2057@gmail.com>
2025-04-14 13:49:08 +0545

    add auto refreshing access token mechanism

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { userAuthStore } from "../store";
 export const api=axios.create(
   {
     baseURL:import.meta.env.VITE_BACKEND_API_URL,
@@ -9,5 +9,35 @@ export const api=axios.create(
       Accept:'application/json',
     }
 
+  });
+
+const refreshToken=async()=>{
+return await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,{},{withCredentials:true,});
+
+};
+//Due to post request we need to pass the empty object as data in the request body,
+//To avoid circular dependency, we are not importing the refresh token function here
+
+api.interceptors.response.use((response)=>response,async (error)=>{
+  const originalRequest=error.config;
+
+  if(error.response?.status===401 && !originalRequest._isRetry){
+    try {
+      originalRequest._isRetry=true;
+      const headers={
+        ...originalRequest.headers,};
+      await refreshToken();
+      console.log("Token refreshed");
+      return api.request({...originalRequest,headers});
+    } catch (err) {
+      console.error("Token refresh error",err);
+      //logout or redirect to login page
+      //window.location.href="http://clientui/login" OR,
+      userAuthStore.getState().logout();
+      return Promise.reject(err);
+
+    }
   }
-);
+
+  return Promise.reject(error);
+})
