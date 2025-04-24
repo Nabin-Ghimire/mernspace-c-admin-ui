d8297d944fcf96f66f1b80ff5b51a83515ceaf6b
Nabin Ghimire <ghimiren2057@gmail.com>
2025-04-24 14:06:39 +0545

    creating tenant and getting tenants in paginated form

--- src/pages/Tenants/Tenants.tsx
+++ src/pages/Tenants/Tenants.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import { userAuthStore } from '../../store'
 import { Link, Navigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { getTenants } from '../../http/api';
-import { Breadcrumb, Button, Drawer, Space, Table } from 'antd';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createTenant, getTenants } from '../../http/api';
+import { Breadcrumb, Button, Drawer, Form, Space, Table } from 'antd';
 import { PlusOutlined, RightOutlined } from '@ant-design/icons';
+import TenantForm from './forms/TenantForm';
 import TenantFilter from './TenantFilter';
+import { CreateTenantData } from '../../types';
+import { PER_PAGE } from '../../constants';
 
 const columns = [
   {
@@ -23,7 +26,14 @@ const columns = [
 ]
 
 const Tenants = () => {
+
+  const [form] = Form.useForm();
+  const queryClient = useQueryClient();
   const [opentenantDrawer, setOpentenantDrawer] = useState(false);
+  const [queryParams, setQueryParams] = useState({
+    perPage: PER_PAGE,
+    currentPage: 1,
+  })
 
   const { user }
     = userAuthStore();
@@ -31,10 +41,30 @@ const Tenants = () => {
     <Navigate to='/' replace={true} />
   }
 
+  const onHandleSubmit = async () => {
+    await form.validateFields();
+    await tenantMutate(form.getFieldsValue());
+    form.resetFields();
+    setOpentenantDrawer(false);
+
+
+  }
+
+  const { mutate: tenantMutate } = useMutation({
+    mutationKey: ['tenant'],
+    mutationFn: async (data: CreateTenantData) => createTenant(data).then((res) => res.data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tenants'] })
+    }
+
+  })
+
   const { data: tenants, isLoading, isError, error } = useQuery({
-    queryKey: ['tenants'],
+    queryKey: ['tenants', queryParams],
     queryFn: () => {
-      return getTenants().then((res) => res.data);
+      const queryString = new URLSearchParams(queryParams as unknown as Record<string, string>).toString();
+      return getTenants(queryString).then((res) => res.data);
+
     }
   })
 
@@ -61,17 +91,31 @@ const Tenants = () => {
         >Add Tenant</Button>
       </TenantFilter>
 
-      <Table columns={columns} dataSource={tenants} rowKey={"id"} />
+      <Table columns={columns} dataSource={tenants?.data} rowKey={"id"}
+        pagination={
+          {
+            total: tenants?.total,
+            current: queryParams?.currentPage,
+            pageSize: queryParams.perPage,
+            onChange: (page) =>
+              setQueryParams((prev) => {
+                return {
+                  ...prev,
+                  currentPage: page
+                }
+              })
+          }
+        } />
 
       <Drawer title="Create Tenant" open={opentenantDrawer} onClose={() => setOpentenantDrawer(false)} width={720} destroyOnClose={true} extra={
         <Space>
           <Button onClick={() => { setOpentenantDrawer(false) }}>Cancel</Button>
-          <Button type='primary'>Submit</Button>
+          <Button type='primary' onClick={onHandleSubmit}>Submit</Button>
         </Space>
       }>
-        <p>Create Tenant Here with the Item properties</p>
-        <p>Create Tenant Here with the Item properties</p>
-        <p>Create Tenant Here with the Item properties</p>
+        <Form layout='vertical' form={form} >
+          <TenantForm />
+        </Form>
       </Drawer>
 
     </Space>
