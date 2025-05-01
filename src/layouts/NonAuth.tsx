75b820334063cf09eade8c176007c87246513b02
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-01 13:43:35 +0545

    get already set url query param for redirect to same page after refreshing and getting user

--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,10 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
 import { userAuthStore } from "../store"
 
 const NonAuth = () => {
+  const location = useLocation();
   const { user } = userAuthStore();
   if (user !== null) {
-    return <Navigate to="/" replace={true} />
+
+    const returnTo = new URLSearchParams(location.search).get('returnTo') || '/';
+    return <Navigate to={returnTo} replace={true} />
   }
   return (
     <div>
