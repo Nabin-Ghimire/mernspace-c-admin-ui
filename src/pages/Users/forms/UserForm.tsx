e4aa9e93d2682c4629564cca4adda617cf4686a7
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-02 23:14:01 +0545

    hide select tenant for admin user

--- src/pages/Users/forms/UserForm.tsx
+++ src/pages/Users/forms/UserForm.tsx
@@ -5,6 +5,7 @@ import { api } from '../../../http/client';
 
 
 const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
+  const selectedRole = Form.useWatch('role');
   const { data: tenants } = useQuery({
     queryKey: ['tenants'],
     queryFn: () => api.get('/tenants/dropdown').then((res) => res.data)
@@ -94,30 +95,33 @@ const UserForm = ({ isEditMode = false }: { isEditMode: boolean }) => {
                   placeholder='Select Role'>
 
                   <Select.Option value="admin">Admin</Select.Option>
-                  <Select.Option value="customer">Customer</Select.Option>
                   <Select.Option value="manager">Manager</Select.Option>
                 </Select>
               </Form.Item>
             </Col>
 
-            <Col span={12} >
-              <Form.Item name='tenantId' label='Restaurant'
-                rules={[
-                  {
-                    required: true,
-                    message: 'Restaurant is required'
-                  }
-                ]}>
-                <Select
-                  size='large' style={{ width: '100%' }}
-                  onChange={() => { }}
-                  placeholder='Select Restaurant'>
-                  {
-                    tenants?.tenants.map((tenant: Tenant) => (<Select.Option value={tenant.id}>{tenant.name}</Select.Option>))
-                  }
-                </Select>
-              </Form.Item>
-            </Col>
+            {
+              selectedRole === 'manager' && (
+                <Col span={12} >
+                  <Form.Item name='tenantId' label='Restaurant'
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Restaurant is required'
+                      }
+                    ]}>
+                    <Select
+                      size='large' style={{ width: '100%' }}
+                      onChange={() => { }}
+                      placeholder='Select Restaurant'>
+                      {
+                        tenants?.tenants.map((tenant: Tenant) => (<Select.Option value={tenant.id}>{tenant.name}</Select.Option>))
+                      }
+                    </Select>
+                  </Form.Item>
+                </Col>
+              )
+            }
 
           </Row>
         </Card>
