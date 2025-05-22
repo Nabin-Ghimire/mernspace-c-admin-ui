90210cf86a953c297afc6cab56c50d420ad0c90f
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-22 15:46:54 +0545

    Category type with all fields

--- src/types.ts
+++ src/types.ts
@@ -39,10 +39,27 @@ export type FieldData={
   value?:string;
 }
 
-export type Category={
-  _id:string;
-  name:string;
+export interface PriceConfiguration {
+    [key: string]: {
+        priceType: "base" | "aditional";
+        availableOptions: string[];
+    };
 }
+
+export interface Attribute {
+    name: string;
+    widgetType: "switch" | "radio";
+    defaultValue: string;
+    availableOptions: string[];
+}
+
+export interface Category {
+    _id:string;
+    name: string;
+    priceConfiguration: PriceConfiguration;
+    attributes: Attribute[];
+}
+
 
 export type Product={
   _id:string;
