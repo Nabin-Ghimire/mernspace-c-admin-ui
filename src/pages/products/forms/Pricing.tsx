0232d27d2dccdefbccb9084112b27b29e522cea5
Nabin Ghimire <ghimiren2057@gmail.com>
2025-05-22 15:47:15 +0545

    Pricing component

--- src/pages/products/forms/Pricing.tsx
+++ src/pages/products/forms/Pricing.tsx
@@ -1,6 +1,45 @@
-const Pricing = () => {
+import { Card, Col, Form, InputNumber, Row, Space, Typography } from "antd"
+import { Category } from "../../../types"
+
+type PricingProps = {
+  selectedCategory: string
+}
+
+const Pricing = ({ selectedCategory }: PricingProps) => {
+  const category: Category | null = selectedCategory ? JSON.parse(selectedCategory) : null;
+  if (!category) {
+    return null;
+  }
   return (
-    <div>Pricing</div>
+    <Card title={<Typography.Text>Product price</Typography.Text>}>
+      {
+        Object.entries(category.priceConfiguration).map(([configurationKey, configurationValue]) => {
+          return <div key={configurationKey}>
+            <Space direction="vertical" size='large' style={{ width: '100%' }}>
+              <Typography.Text>{`${configurationKey} (${configurationValue.priceType})`}</Typography.Text>
+
+              <Row gutter={20}>
+                {
+                  configurationValue.availableOptions.map((option: string) => {
+                    return (
+                      <Col span={8} key={option}>
+                        <Form.Item label={option} name={['priceConfiguration', JSON.stringify({ configurationKey: configurationKey, priceType: configurationValue.priceType, }),
+                          option
+                        ]}>
+
+                          <InputNumber addonAfter='रु' />
+
+                        </Form.Item>
+                      </Col>
+                    )
+                  })
+                }
+              </Row>
+            </Space>
+          </div>
+        })
+      }
+    </Card>
   )
 }
 
